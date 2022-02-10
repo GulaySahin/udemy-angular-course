7d21df8d55c114c562f94ae378f5c731a402bb33
ETIYACORP\gulay.sahin <gulay.sahin@etiya.com>
2022-02-10 14:42:48 +0300

    Giriş-Projenin oluşturulması-Paketlerin eklenmesi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'udemy-angular-course';
+
+  constructor(private toastr: ToastrService,
+    private spinner:NgxSpinnerService
+    ) {}
+  ngOnInit() {
+  this.showSuccess();
+  this.showSpinner();
+  }
+
+  showSuccess() {
+    this.toastr.success('Udemy Angular 9 projemize hoşgeldiniz..', 'Giriş başarılı');
+  }
+  showSpinner(){
+    this.spinner.show();
+
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 5000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from "ngx-spinner";
+
+import { IgxButtonModule } from 'igniteui-angular';
 
 @NgModule({
   declarations: [
@@ -10,9 +18,24 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot({
+      timeOut: 4000,
+      progressBar: true,
+      easing: "ease-in",
+      closeButton: false,
+      progressAnimation: "decreasing",
+      preventDuplicates: true,
+      positionClass: "toast-bottom-left"
+    }),
+    NgxSpinnerModule,
+    IgxButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+}
